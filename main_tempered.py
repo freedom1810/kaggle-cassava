0dc8d17b34bb11c0c3baf529b425d33bc874a433
hainamnguyen <hainamnguyen.th.vn@gmail.com>
2021-02-04 07:27:58 +0700

    folcal cosin loss

--- main_tempered.py
+++ main_tempered.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from sklearn.model_selection import StratifiedKFold
 from data import CassaDataset
 from nets_best import EfficientNetB3DSPlus, ModelEMA
-from losses import LabelSmoothingLoss, SCELoss
+from losses import LabelSmoothingLoss, SCELoss, FocalCosineLoss
 from engines_tempered_fp16 import trainer_augment
 from torchcontrib.optim import SWA
 from albumentations.pytorch import ToTensorV2
@@ -25,8 +25,7 @@ import gc
 
 
 path_params = {
-    # 'csv_path': "/home/hana/sonnh/kaggle-cassava/dataset/train_mix/new_mix_1234.csv",
-    'csv_path': "/home/hana/sonnh/kaggle-cassava/dataset/train_mix/new_mix_v3.csv",
+    'csv_path': "/home/hana/sonnh/kaggle-cassava/dataset/train_mix/new_mix_1234.csv",
     'img_path': "/home/hana/sonnh/kaggle-cassava/dataset/original_mix/",
     'save_path': "checkpoints/64/{}_fold-{}"
 
@@ -50,7 +49,7 @@ optimizer_params = {
     'weighted_loss': True,
     # 'weight_loss': torch.tensor([1.0, 0.43, 0.49, 0.1, 0.52]),
     'weight_loss': None,
-    'lrf': 1e-3
+    'lrf': 1e-2
 }
 
 training_params = {
@@ -59,14 +58,18 @@ training_params = {
     'device': torch.device("cuda:0"),
     'device_ids': [0, 1],
     'start_epoch': 1,
-    'num_epoch': 75,
+    'num_epoch': 50,
     'warm_up': 5,
     'TTA_time': 5
 }
 
 df = pd.read_csv(path_params['csv_path'])
 
-for fold in [1, 2, 3, 4,5]:
+for fold in [2]:
+    # for mean, std in [[[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]], 
+    #                 [[0.4342, 0.4967, 0.3154],[0.2115, 0.2157, 0.1913]]]: 
+    # print(' mean {} std {}'.format(mean, std))
+
     """StratifiedKFold"""
     print("="*20, "Fold", fold, "="*20)
     train_df = df[df["fold"] != fold].reset_index(drop=True)
@@ -86,8 +89,9 @@ for fold in [1, 2, 3, 4,5]:
             ShiftScaleRotate(p=0.5),
             HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
             RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.5),
-            # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
-            Normalize(mean=[0.4342, 0.4967, 0.3154], std=[0.2115, 0.2157, 0.1913], max_pixel_value=255.0, p=1.0),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
+            # Normalize(mean=[0.4342, 0.4967, 0.3154], std=[0.2115, 0.2157, 0.1913], max_pixel_value=255.0, p=1.0),
+            # Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
             #Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0, p=1.0),
             CoarseDropout(p=0.5),
             Cutout(p=0.5),
@@ -115,8 +119,9 @@ for fold in [1, 2, 3, 4,5]:
         ShiftScaleRotate(p=0.5),
         HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
         RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.5),
-        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
-        Normalize(mean=[0.4342, 0.4967, 0.3154], std=[0.2115, 0.2157, 0.1913],max_pixel_value=255.0, p=1.0),
+        Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
+        # Normalize(mean=[0.4342, 0.4967, 0.3154], std=[0.2115, 0.2157, 0.1913],max_pixel_value=255.0, p=1.0),
+        # Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
         #Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0, p=1.0),
         #CoarseDropout(p=0.5),
         #Cutout(p=0.5),
@@ -143,11 +148,14 @@ for fold in [1, 2, 3, 4,5]:
     if model_params['EMA']:
         model_params['ema_model'] = ModelEMA(model)
     if optimizer_params['weighted_loss']:
-        criterion = SCELoss()
+        # criterion = SCELoss()
+        criterion = FocalCosineLoss()
         val_criterion = LabelSmoothingLoss(smoothing = 0, weight = optimizer_params['weight_loss'], training = False)
+
     optimizer = optim.Adam(model.parameters(), lr=5e-4)
     # optimizer = optim.SGD
     # optimizer = SAM(model.parameters(), optimizer, lr=0.1, momentum=0.9)
+
     #lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=.1)
     lf = lambda x: ((1 + math.cos(x * math.pi / training_params['num_epoch'])) / 2) * (1 - optimizer_params['lrf']) + optimizer_params['lrf']  # cosine
     lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
